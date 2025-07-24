d17f3058f3572fd78173211d5573a4e618c29c43
Snehal Kedia <kediasnehal73@gmail.com>
2025-07-24 17:03:38 +0000

    added

--- main.tf
+++ main.tf
@@ -1,58 +1,58 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
+# VPC
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "main-vpc"
-  }
+  cidr_block = var.vpc_cidr
 }
 
-resource "aws_subnet" "main" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "main-subnet"
-  }
-}
-
-resource "aws_internet_gateway" "gw" {
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main-gw"
-  }
 }
 
-resource "aws_route_table" "main" {
+# Public Subnets
+resource "aws_subnet" "public" {
+  count             = 3
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidrs[count.index]
+  map_public_ip_on_launch = true
+  availability_zone = "${var.aws_region}a"
+}
+
+# Private Subnets
+resource "aws_subnet" "private" {
+  count             = 3
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = "${var.aws_region}b"
+}
+
+# Route Table for Public Subnet
+resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  tags = {
-    Name = "main-route-table"
+    gateway_id = aws_internet_gateway.igw.id
   }
 }
 
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.main.id
+# Route Table Association
+resource "aws_route_table_association" "public_assoc" {
+  count          = 3
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH and HTTP"
+# Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow 80, 22, 8080"
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description = "Allow SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -60,9 +60,15 @@ resource "aws_security_group" "allow_ssh" {
   }
 
   ingress {
-    description = "Allow HTTP"
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -73,21 +79,17 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "allow-ssh"
-  }
 }
 
+# EC2 Instance in Public Subnet
 resource "aws_instance" "ubuntu_instance" {
-  ami                         = "ami-053b0d53c279acc90" # Ubuntu 22.04 LTS (us-east-1)
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.main.id
-  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
-  associate_public_ip_address = true
-  key_name                    = "newk"
+  ami                    = "ami-0c7217cdde317cfec" # Ubuntu AMI for us-east-1
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public[0].id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name               = var.key_pair_name
 
   tags = {
-    Name = "ubuntu-ec2"
+    Name = "Terraform-Ubuntu-Instance"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,19 @@
 variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
-  default     = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
 variable "key_pair_name" {
-  description = "Name of the EC2 Key Pair"
-  type        = string
-  default     = "newk"  # Optional, only if you're fixing this
+  description = "EC2 key pair name"
 }
